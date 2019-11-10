9daed99ae41d13ebf1511c3be571f7c8d0c6bc10
Nelson W Foltz <nelsonwfoltz@Nelsons-iMac.fios-router.home>
2019-11-10 14:25:57 -0500

    implemented fake asynch validator for name input

--- src/app/signup-form/username.validators.ts
+++ src/app/signup-form/username.validators.ts
@@ -10,4 +10,17 @@ export class UsernameValidators {
       return null;
     }
   }
+  static shouldBeUnique(
+    control: AbstractControl
+  ): Promise<ValidationErrors | null> {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'mosh') {
+          resolve({ shouldBeUnique: true });
+        } else {
+          resolve(null);
+        }
+      }, 2000);
+    });
+  }
 }
