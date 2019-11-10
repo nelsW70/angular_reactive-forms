0dcca0df14c4b9f11ae74c8ef5dbb4e5dda54ec1
Nelson W Foltz <nelsonwfoltz@Nelsons-iMac.fios-router.home>
2019-11-10 12:42:40 -0500

    adding validation to username input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { SignupFormComponent } from './signup-form/signup-form.component';
 
 @NgModule({
   declarations: [AppComponent, SignupFormComponent],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
